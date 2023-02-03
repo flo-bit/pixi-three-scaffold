7e530234f712c7bd686b6f98528713c25eba67db
florian <45694132+flo-bit@users.noreply.github.com>
2023-02-03 23:47:40 +0100

    change to extend pixi-scaffold

--- pixi-three-scaffold.js
+++ pixi-three-scaffold.js
@@ -1,36 +1,12 @@
-import * as PIXI from "https://cdn.jsdelivr.net/npm/pixi.js@7.x/dist/pixi.min.mjs";
 import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js";
+import PixiScaffold from "https://flo-bit.github.io/pixi-scaffold/pixi-scaffold.js";
 
-export default class PixiThreeScaffold {
-  constructor(opts) {
-    this.opts = opts ?? {};
-    window.PIXI = PIXI;
-    this.opts.PS = this;
-    window.PS = this;
-    this.app = new PIXI.Application();
-    this.app.view.style.position = "absolute";
-    this.app.view.style.top = "0px";
-    this.app.view.style.left = "0px";
-    document.body.appendChild(this.app.view);
-
-    this.w = this.w ?? 1000;
-    this.h = this.h ?? 1000;
-    this.root = new PIXI.Container();
-    this.root.sortableChildren = true;
-    this.app.stage.addChild(this.root);
-    this.windowResized();
-
-    this.keys = {};
-    document.addEventListener("keydown", this.keyDown.bind(this));
-    document.addEventListener("keyup", this.keyUp.bind(this));
-    window.addEventListener("resize", this.windowResized.bind(this));
-
+export default class PixiThreeScaffold extends PixiScaffold {
+  setup() {
     this.setupThree();
-
-    if (this.opts.setup) this.opts.setup(this);
-    let ticker = PIXI.Ticker.shared;
-    ticker.add(this.animate.bind(this));
+    super.setup();
   }
+
   setupThree() {
     window.THREE = THREE;
 
@@ -88,39 +64,10 @@ export default class PixiThreeScaffold {
   }
 
   animate() {
-    /* Update your scene here */
-    let elapsed = PIXI.Ticker.shared.elapsedMS / 1000.0;
-    let total = PIXI.Ticker.shared.lastTime / 1000.0;
-
     if (this.three) {
       this.three.renderer.render(this.three.scene, this.three.camera);
       this.three.texture.update();
     }
-    if (this.opts.update) this.opts.update(elapsed, total, this);
-  }
-  resizeRoot() {
-    let w = window.innerWidth,
-      h = window.innerHeight;
-    let scl = Math.min(w / this.w, h / this.h);
-    this.root.position.x = w / 2;
-    this.root.position.y = h / 2;
-    this.root.scale.x = scl;
-    this.root.scale.y = scl;
-  }
-  windowResized() {
-    this.app.renderer.resize(window.innerWidth, window.innerHeight);
-    this.resizeRoot();
-  }
-  keyDown(event) {
-    this.keys[event.key] = true;
-  }
-  keyUp(event) {
-    this.keys[event.key] = false;
-  }
-  addChild(c) {
-    this.root.addChild(c);
-  }
-  add(c) {
-    this.addChild(c);
+    super.animate();
   }
 }
